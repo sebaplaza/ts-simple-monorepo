ca819ba094e92b855f04bd264a366ccf4a055447
Sebastian Plaza <sebastianplaza@gmail.com>
2021-11-14 20:47:38 +0100

    feat: first version

---
+++ packages/client/src/index.ts
@@ -0,0 +1,5 @@
+import sayhi from '@spa/greeter'
+
+const res = sayhi('spa');
+
+console.log(res)
---
+++ packages/greeter/src/index.ts
@@ -0,0 +1,9 @@
+
+
+const world = 'world';
+
+function hello(world: string = 'world'): string {
+    return `Hello ${world}! ...`;
+}
+
+export default hello; 
